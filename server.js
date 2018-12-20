6fbd19608df5830b8e71da909f496813f587d8ce
Noumanmufc1 <noumanmfc07@gmail.com>
2018-12-20 13:00:19 +0500

    added deployment configuration

--- server.js
+++ server.js
@@ -26,14 +26,15 @@ app.use("/", userRoutes)
 app.use("/challenges", challengeRoutes)
 const path = require("path")
 
-
-app.use(express.static(path.join(__dirname, "client", "build")))
-
-// ...
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+  
 app.listen(port, function() {
     console.log('Server listening')
 })
